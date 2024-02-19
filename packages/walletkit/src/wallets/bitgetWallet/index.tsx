0a778808e799086e77c36ffb340911f9fbc4e460
BitgetWalletTeam <158799510+BitgetWalletTeam@users.noreply.github.com>
2024-02-19 11:34:31 +0800

    Bitget wallet (#108)
    * feat: integrate bitget wallet
    
    * provider
    
    ---------
    
    Co-authored-by: Jeff <jeff.xue@bitget.com>

---
+++ packages/walletkit/src/wallets/bitgetWallet/index.tsx
@@ -0,0 +1,65 @@
+import { Chain } from 'wagmi';
+import { PartialCustomProps, WalletProps } from '..';
+import { CustomConnector } from '../custom/connector';
+import { getInjectedProvider, hasInjectedProvider } from '../utils';
+import { BitgetWalletIcon, BitgetWalletTransparentIcon } from './icon';
+import { isMobile } from '@/index';
+
+export const BITGET_WALLET_ID = 'bitgetWallet';
+export const BITGET_WALLET_NAME = 'Bitget Wallet';
+
+export function bitgetWallet(props: PartialCustomProps = {}): WalletProps {
+  const { connectorOptions, ...restProps } = props;
+
+  return {
+    id: BITGET_WALLET_ID,
+    name: BITGET_WALLET_NAME,
+    logos: {
+      default: <BitgetWalletIcon />,
+      transparent: <BitgetWalletTransparentIcon />,
+    },
+    downloadUrls: {
+      default: 'https://web3.bitget.com/',
+    },
+    spinnerColor: undefined,
+    showQRCode: false,
+    isInstalled: isBitgetWallet,
+    createConnector: (chains: Chain[]) => {
+      return new CustomConnector({
+        id: BITGET_WALLET_ID,
+        chains,
+        options: {
+          name: BITGET_WALLET_NAME,
+          shimDisconnect: true,
+          getProvider() {
+            if (typeof window === 'undefined') return;
+
+            if (isMobile()) {
+              return window.ethereum || window.bitkeep.ethereum;
+            }
+
+            return getInjectedProvider('isBitgetWallet') ?? window.bitkeep.ethereum;
+          },
+          ...connectorOptions,
+        },
+      });
+    },
+    getDeepLink: () => {
+      return `https://bkcode.vip?action=dapp&url=${window.location.href}`;
+    },
+    getQRCodeUri(uri) {
+      return `bitkeep://bkconnect/wc?uri=${encodeURIComponent(uri)}`;
+    },
+    ...restProps,
+  };
+}
+
+export function isBitgetWallet() {
+  if (typeof window === 'undefined') return false;
+
+  if (isMobile()) {
+    return !!(window.ethereum || window.bitkeep);
+  }
+
+  return hasInjectedProvider('isBitgetWallet') || window.bitkeep;
+}
