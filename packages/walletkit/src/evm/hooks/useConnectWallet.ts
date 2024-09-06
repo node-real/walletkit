69e6f4edb2708d70cb596b639fe6f5bbd8bb2b3e
Wenty Li <105278450+wenty22@users.noreply.github.com>
2024-09-06 13:05:26 +0800

    feat: Support async connect a wallet (#211)

--- packages/walletkit/src/evm/hooks/useConnectWallet.ts
+++ packages/walletkit/src/evm/hooks/useConnectWallet.ts
@@ -1,24 +1,46 @@
-import { useEvmConnect } from '@/evm/hooks/useEvmConnect';
+import { useEvmConnect, UseEvmConnectReturnType } from '@/evm/hooks/useEvmConnect';
 import { useWalletKit } from '../../core/providers/WalletKitProvider/context';
+import { useIsConnected } from './useIsConnected';
+
+type ConnectOptions = Partial<Parameters<UseEvmConnectReturnType['connect']>[0]> & {
+  walletId: string;
+};
 
 export function useConnectWallet() {
   const { log, evmConfig } = useWalletKit();
 
-  const { connect, connectors } = useEvmConnect();
+  const { connect, connectAsync, connectors } = useEvmConnect();
+  const isConnected = useIsConnected();
 
   return {
-    connect(options: { walletId: string; initialChainId?: number }) {
-      const { walletId, initialChainId } = options;
+    connect(options: ConnectOptions) {
+      const { walletId, ...restOptions } = options;
       const wallet = evmConfig?.wallets.find((item) => item.id === walletId);
 
       if (!wallet) {
         log(`wallet not found, walletId: ${walletId}`);
       } else {
         const connector = connectors.find((item) => item.id === walletId);
-        if (connector && wallet.isInstalled())
+        if (connector && wallet.isInstalled() && !isConnected)
           connect({
             connector,
-            chainId: initialChainId,
+            ...restOptions,
+          });
+      }
+    },
+
+    async connectAsync(options: ConnectOptions) {
+      const { walletId, ...restOptions } = options;
+      const wallet = evmConfig?.wallets.find((item) => item.id === walletId);
+
+      if (!wallet) {
+        log(`wallet not found, walletId: ${walletId}`);
+      } else {
+        const connector = connectors.find((item) => item.id === walletId);
+        if (connector && wallet.isInstalled() && !isConnected)
+          await connectAsync({
+            connector,
+            ...restOptions,
           });
       }
     },
